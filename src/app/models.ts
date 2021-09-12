b6b92d8cf7d892179797ba8bb27d8d783d2e5e34
darkjazz <alo@anrita.local>
2021-09-12 17:18:16 +0100

    added models

---
+++ src/app/models.ts
@@ -0,0 +1,20 @@
+export class Audio {
+  title: string;
+  embed_url: string;
+}
+
+export class SourceAttributes {
+  url:string;
+  color:string;
+  auto_play:boolean;
+  hide_related:boolean;
+  show_comments:boolean;
+  show_user:boolean;
+  show_reposts:boolean;
+  show_teaser:boolean;
+  visual:boolean;
+}
+
+export class Header {
+  greeting: string;
+}
